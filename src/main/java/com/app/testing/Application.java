149a255c455e97b4f92e72ea9c60bc2d39bb76c1
Dmytro Korytov <dmytrokorytov@Dmytros-MacBook-Pro.local>
2021-02-26 10:55:23 +0200

    Added database credentials

--- src/main/java/com/app/testing/Application.java
+++ src/main/java/com/app/testing/Application.java
@@ -1,9 +1,13 @@
 package com.app.testing;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.app.testing")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
